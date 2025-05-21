e2a14dc61b6a0647fd2113b45b067b8bea58040d
seimes <simon.wolfsteiner@a1.net>
2025-05-21 10:57:05 +0200

    Fixed the issue with reloading the regisiter page when other is selected.

--- src/main/resources/static/js/form-logic.js
+++ src/main/resources/static/js/form-logic.js
@@ -2,11 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleSalutationSelect = document.getElementById('salutation');
     const otherInput = document.getElementById('salutation-other-group');
 
-    toggleSalutationSelect.addEventListener('change', function () {
-        if (this.value === 'other') {
+    // Function to show/hide based on current value
+    const updateVisibility = () => {
+        if (toggleSalutationSelect.value === 'other') {
             otherInput.style.display = 'block';
         } else {
             otherInput.style.display = 'none';
         }
-    });
+    };
+
+    // Call once on load
+    updateVisibility();
+
+    // And again when changed
+    toggleSalutationSelect.addEventListener('change', updateVisibility);
 });

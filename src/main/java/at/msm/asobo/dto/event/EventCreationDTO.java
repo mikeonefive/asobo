005bce06bb888c9396a134cf803d512c14c84a05
seimes <simon.wolfsteiner@a1.net>
2025-06-13 10:31:08 +0200

    Added validation to EventCreationDTO

--- src/main/java/at/msm/asobo/dto/event/EventCreationDTO.java
+++ src/main/java/at/msm/asobo/dto/event/EventCreationDTO.java
@@ -3,7 +3,8 @@ package at.msm.asobo.dto.event;
 import at.msm.asobo.dto.medium.MediumDTO;
 import at.msm.asobo.dto.comment.UserCommentDTO;
 import at.msm.asobo.dto.user.UserDTO;
-import at.msm.asobo.entities.Event;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,20 +13,25 @@ import java.util.UUID;
 public class EventCreationDTO {
     private UUID id;
 
+    @NotBlank(message = "Title is mandatory for event creation")
     private String title;
 
+    @NotBlank(message = "Description is mandatory for event creation")
     private String description;
 
     private URI pictureURI;
 
+    @NotBlank(message = "Location is mandatory for event creation")
     private String location;
 
+    @NotNull(message = "Date is mandatory for event creation")
     private LocalDateTime date;
 
     private LocalDateTime creationDate;
 
     private LocalDateTime modificationDate;
 
+    @NotNull(message = "Event creator is mandatory for event creation")
     private EventCreatorDTO creator;
 
     private List<UserDTO> participants;

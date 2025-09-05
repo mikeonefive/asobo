8d17a2ddeac34dc79c05c6604624a5a67a953ca1
Mike OneFive <notsure@riseup.net>
2025-09-05 15:19:53 +0200

    fix: thumbnail gallery order

--- backend/src/main/java/at/msm/asobo/controllers/UserController.java
+++ backend/src/main/java/at/msm/asobo/controllers/UserController.java
@@ -1,10 +1,8 @@
 package at.msm.asobo.controllers;
 
-import at.msm.asobo.dto.user.UserDTO;
 import at.msm.asobo.dto.user.UserPublicDTO;
 import at.msm.asobo.dto.user.UserRegisterDTO;
 import at.msm.asobo.dto.user.UserUpdateDTO;
-import at.msm.asobo.entities.User;
 import at.msm.asobo.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;

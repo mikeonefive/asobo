7becabf1e483d72057ba6cc27e694e16dac92757
Mike OneFive <notsure@riseup.net>
2025-09-17 18:46:32 +0200

    header & footer tryouts

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {

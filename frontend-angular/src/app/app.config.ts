63b28ae1116c2e56ebe01a35ed0b40c7e9c75a2c
Simon Wolfsteiner <seimes@riseup.net>
2025-10-12 02:20:37 +0200

    feat: added primeng themes and improved registration and login forms

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {AuthInterceptor} from './core/interceptors/auth-interceptor';
@@ -14,5 +15,13 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     AuthInterceptor,
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false // or '.dark-mode' if you want dark mode support
+        }
+      }
+    })
   ]
 };

17e147b38a3e5a2ba162f21b4847811ab8ba3f8a
Simon Wolfsteiner <seimes@riseup.net>
2025-10-18 00:53:49 +0200

    feat: reimplemented about page in angular

--- frontend-angular/src/app/core/about/about-page/about-page.ts
+++ frontend-angular/src/app/core/about/about-page/about-page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import {UrlUtilService} from '../../../shared/utils/url/url-util-service';
 
 @Component({
   selector: 'app-about-page',
@@ -8,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class AboutPage {
 
+  protected readonly environment = environment;
+  protected readonly UrlUtilService = UrlUtilService;
 }

a716b486a71d9ce840aa83a26fb78105de1d048b
Mike Onefive <notsure@riseup.net>
2025-11-19 15:17:11 +0100

    #63 feat: basic primeNG list for users

--- frontend-angular/src/app/features/admin/admin-user-list/admin-user-list.ts
+++ frontend-angular/src/app/features/admin/admin-user-list/admin-user-list.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { User } from '../../auth/models/user';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin-user-list',
-  imports: [],
+  imports: [
+    TableModule,
+    TagModule,
+    DatePipe
+  ],
   templateUrl: './admin-user-list.html',
   styleUrl: './admin-user-list.scss',
 })
 export class AdminUserList {
-
+  users: User[] = [];
 }
